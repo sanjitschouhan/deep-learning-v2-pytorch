21bfdba1cdffc6687a3527bc6da59fe54a2c6964
Sanjit Singh Chouhan <sanjitschouhan@gmail.com>
2020-10-10 14:26:53 +0530

    submitted project

--- project-bikesharing/my_answers.py
+++ project-bikesharing/my_answers.py
@@ -140,7 +140,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 1000
+iterations = 3000
 learning_rate = 1.0
 hidden_nodes = 10
 output_nodes = 1
